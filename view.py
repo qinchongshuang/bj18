dbbc935c4548c9958a5127536982d3a3c7375f00
qinchongshuang <474285794@qq.com>
2018-12-10 16:51:48 +0800

    创建login视图

--- view.py
+++ view.py
@@ -1,5 +1,9 @@
 from django.http import Httpresponse
+from django.shortcuts import redirect
+
 
 def index(request):
     return Httpresponse("index")
 
+def login(request):
+    return redirect('/index')
